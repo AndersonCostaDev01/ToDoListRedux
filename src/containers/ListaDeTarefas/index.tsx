7a57bcf14c4d080a6bd6e4cc9c801a61e4cc344f
Anderson Costa <costaandersom23@gmail.com>
2024-11-13 17:15:35 -0300

    Criação da funcionalidade de edição dos campos

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -5,15 +5,16 @@ import { Container } from './styles'
 import { RootReducer } from '../../store'
 
 const ListaDeTarefas = () => {
-  const { tarefas } = useSelector((state: RootReducer) => state)
+  const { itens } = useSelector((state: RootReducer) => state.tarefas)
   return (
     <Container>
       <p>2 Tarefas marcadas como: &quot;Categoria&quot; e &quot;Termo&quot;</p>
       <ul>
-        {tarefas.map((tarefa) => (
+        {itens.map((tarefa) => (
           <li key={tarefa.titulo}>
             <Titulo>{tarefa.titulo}</Titulo>
             <Tarefa
+              id={tarefa.id}
               prioridade={tarefa.prioridade}
               status={tarefa.status}
               titulo={tarefa.titulo}
--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -2,37 +2,60 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import Tarefa from '../../models/Tarefa'
 import * as enums from '../../utils/enums/Tarefas'
 
+type TarefasState = {
+  itens: Tarefa[]
+}
+
+const initialState: TarefasState = {
+  itens: [
+    {
+      id: 1,
+      titulo: 'Estudar Typescript',
+      prioridade: enums.Prioridade.URGENTE,
+      status: enums.Status.PENDENTE,
+      descricao: 'Estudar Typescript para atividade da semana'
+    },
+    {
+      id: 2,
+      titulo: 'Estudar React',
+      prioridade: enums.Prioridade.URGENTE,
+      status: enums.Status.CONCLUIDA,
+      descricao: 'Estudar React para atividade da semana'
+    },
+    {
+      id: 3,
+      titulo: 'Exame medico',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.PENDENTE,
+      descricao: 'Exame medico para saber sobre estado de saude'
+    },
+    {
+      id: 4,
+      titulo: 'Fazer a janta',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.CONCLUIDA,
+      descricao: 'Fazer a janta para comer apos os estudos '
+    }
+  ]
+}
+
 const tarefasSlice = createSlice({
   name: 'tarefas',
-  initialState: [
-    new Tarefa(
-      'Estudar Typescript',
-      enums.Prioridade.URGENTE,
-      enums.Status.PENDENTE,
-      'Estudar Typescript para atividade da semana',
-      1
-    ),
-    new Tarefa(
-      'Estudar React',
-      enums.Prioridade.URGENTE,
-      enums.Status.CONCLUIDA,
-      'Estudar React para atividade da semana',
-      2
-    ),
-    new Tarefa(
-      'Exame medico',
-      enums.Prioridade.NORMAL,
-      enums.Status.PENDENTE,
-      'Exame medico para saber sobre estado de saude',
-      3
-    )
-  ] as Tarefa[],
+  initialState,
   reducers: {
     remover: (state, action: PayloadAction<number>) => {
-      state = state.filter((tarefa) => tarefa.id !== action.payload)
+      state.itens = state.itens.filter((tarefa) => tarefa.id !== action.payload)
+    },
+    editar: (state, action: PayloadAction<Tarefa>) => {
+      const indexDaTarefa = state.itens.findIndex(
+        (t) => t.id === action.payload.id
+      )
+      if (indexDaTarefa >= 0) {
+        state.itens[indexDaTarefa] = action.payload
+      }
     }
   }
 })
 
-export const { remover } = tarefasSlice.actions
+export const { remover, editar } = tarefasSlice.actions
 export default tarefasSlice.reducer
